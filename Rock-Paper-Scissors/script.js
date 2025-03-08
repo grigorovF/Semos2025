a23d7d34f2ed3e1db7c527a4ea398a5ed157cc94
Filip Grigorov <filipgrigorov44@gmail.com>
2025-03-08 11:20:06 +0100

    Rock Paper Scissors

---
+++ Rock-Paper-Scissors/script.js
@@ -0,0 +1,37 @@
+var rock = String.fromCodePoint(0x1F44A);
+var paper = String.fromCodePoint(0x1f590);
+var scissors = String.fromCodePoint(0x270c);
+
+var choices = [rock, paper, scissors];
+
+const userResult = document.getElementById('userResult');
+const computerResult = document.getElementById('computerResult');
+const wonSpan = document.getElementById('wonSpan');
+
+const playBtn = document.getElementById('playBtn');
+
+playBtn.addEventListener('click', function(){
+
+    var userIndex = Math.floor(Math.random() * choices.length);
+    var comIndex = Math.floor(Math.random() * choices.length);
+
+
+    var userChoice = choices[userIndex];
+    var computerChoice = choices[comIndex];
+
+    userResult.innerText = "User " + userChoice + " - ";
+    computerResult.innerText = computerChoice + " Computer";
+    
+    if (userChoice === computerChoice)
+        wonSpan.innerText = 'TIE';
+    if ((userChoice === paper && computerChoice == rock) ||
+        (userChoice === rock && computerChoice == scissors) ||
+        (userChoice === scissors && computerChoice == paper)){
+            wonSpan.innerText = 'User Win!';
+        }
+    else if ((computerChoice === paper && userChoice == rock) ||
+            (computerChoice === rock && userChoice == scissors) ||
+            (computerChoice === scissors && userChoice == paper)){
+                wonSpan.innerText = "Computer Win!"
+            }
+});
