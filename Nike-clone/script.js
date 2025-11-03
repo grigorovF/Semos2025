2cb4c28b0e69eb8524e109dc2a5cc91375057055
Filip Grigorov <filipgrigorov44@gmail.com>
2025-11-03 20:27:04 +0100

    nike

---
+++ Nike-clone/script.js
@@ -0,0 +1,105 @@
+// Background Slideshow
+
+const main = document.getElementById("items");
+const lines = document.querySelectorAll(".lines");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+const timerDisplay = document.getElementById("time");
+
+const backgrounds = [
+  "url('img/nike-pegasus-41-21635997-main.webp')",
+  "url('img/12.webp')",
+  "url('img/nike1.webp')",
+  "url('img/sp1pwv9merjbj5wa1kch.jpg')"
+];
+
+let index = 0;
+let timeLeft = 5;
+let timerInterval;
+let slideInterval;
+
+function updateLines() {
+  lines.forEach((line, i) => {
+    if (i === index) {
+      line.classList.add('active', 'line-color'); 
+    } else {
+      line.classList.remove('active', 'line-color');
+    }
+  });
+}
+
+function changeBackground() {
+  main.style.backgroundImage = backgrounds[index];
+  main.style.transition = "background-image 1s ease-in-out";
+  updateLines();
+}
+
+function nextSlide() {
+  index = (index + 1) % backgrounds.length;
+  restartSlideshow();
+}
+
+function prevSlide() {
+  index = (index - 1 + backgrounds.length) % backgrounds.length;
+  restartSlideshow();
+}
+
+function startSlideshow() {
+  clearInterval(slideInterval);
+  clearInterval(timerInterval);
+
+  timeLeft = 5;
+  timerDisplay.textContent = timeLeft;
+
+  changeBackground();
+
+  timerInterval = setInterval(() => {
+    timeLeft--;
+    timerDisplay.textContent = timeLeft;
+    if (timeLeft <= 0) {
+      nextSlide();
+    }
+  }, 1000);
+
+  slideInterval = setInterval(nextSlide, 5000);
+}
+
+function restartSlideshow() {
+  clearInterval(slideInterval);
+  clearInterval(timerInterval);
+  startSlideshow();
+}
+
+left.addEventListener("click", prevSlide);
+right.addEventListener("click", nextSlide);
+
+changeBackground();
+updateLines();
+startSlideshow();
+
+
+//search-togle
+function toggleSearchInput() {
+  const searchInput = document.getElementById("search");
+  if (!searchInput) return;
+
+  if (window.innerWidth <= 990) {
+    searchInput.style.display = "none";
+  } else {
+    searchInput.style.display = "flex";
+  }
+}
+
+window.addEventListener('load', toggleSearchInput);
+window.addEventListener('resize', toggleSearchInput);
+
+
+//show sideList
+function showSideList() {
+  const sideList = document.getElementById("side-list");
+  if (sideList.style.display === "" || sideList.style.display === "none") {
+    sideList.style.display = "flex";
+  } else {
+    sideList.style.display = "none";
+  }
+}
