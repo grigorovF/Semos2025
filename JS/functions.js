af3246a0c76b1d326705969e000aa3f9d970a371
Filip Grigorov <filipgrigorov44@gmail.com>
2025-11-10 15:55:20 +0100

    Functions and objects

---
+++ JS/functions.js
@@ -0,0 +1,83 @@
+//fuctions
+
+function adding(a, b){
+    return a+b;
+}
+
+function substracting(a, b){
+    return a-b;
+}
+
+function multiplication(a, b){
+    return a*b;
+}
+
+function devide(a, b){
+    return (a/b)
+}
+
+function modulation(a, b){
+    return a%b
+}
+
+var a = 6;
+var b = 3;
+
+console.log("6 + 3 = ", adding(a, b))
+console.log("6 - 3 = ", substracting(a, b))
+console.log("6 * 3 = ", multiplication(a, b))
+console.log("6 / 3 = ", devide(a, b))
+console.log("6 % 3 = ", modulation(a, b))
+
+
+
+//objects
+let car1 = {
+    make: "Mercedes-Benz",
+    model: "CLK 500",
+    numberSeats: 5,
+    fuelType: "Hybrid",
+    price: 100000
+}
+
+console.log(car1)
+
+let car2 = {
+    make: "BMW",
+    model: "640 GT",
+    numberSeats: 5,
+    fuelType: "Disel",
+    price: 120000
+}
+
+console.log(car2)
+
+let car3 = {
+    make: "Audi",
+    model: "A8",
+    numberSeats: 4,
+    fuelType: "Petrol",
+    price: 150000
+}
+
+console.log(car3)
+
+let car4 = {
+    make: "Volkswagen",
+    model: "Arteon",
+    numberSeats: 5,
+    fuelType: "Electric",
+    price: 100000
+}
+
+console.log(car4)
+
+let car5 = {
+    make: "Volvo",
+    model: "VC 90",
+    numberSeats: 5,
+    fuelType: "Diesel",
+    price: 180000
+}
+
+console.log(car5)
