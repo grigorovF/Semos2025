b26eb28009b0298c4def29abcad636577c260fc4
Filip Grigorov <filipgrigorov44@gmail.com>
2025-12-16 19:26:41 +0100

    D1

---
+++ Node.js/Day1/script.js
@@ -0,0 +1,21 @@
+//! Node.js
+
+//? Што е Node.js?
+
+// Node.js е околина за извршување на JavaScript код надвор од веб прелистувачот.
+// Изработен е врз база на Chrome's V8 JavaScript engine.
+
+//? Клучни карактеристики на Node.js:
+
+// Single threaded: Node.js користи асиннхрон модел на извршување кој овозможувва тешки операции да се извршуваат во позадина.
+// 
+//! 1. Core modules:
+// http/ assert/ fs/ path/ os/ events/ stream/ util
+
+
+//! 2. Third-party modules:
+// express/ mongoose/ dotenv/ ejs/ morgan/ 
+
+
+//!3. Local modules:
+// customModule.js/ helper.js/ config.js ги креирате сами
