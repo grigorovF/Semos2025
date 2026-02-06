fd2cb95d7fc8ca6ec4ec85fb34dca626e5e73b96
Filip Grigorov <filipgrigorov44@gmail.com>
2026-02-06 19:18:15 +0100

    H8

--- Node.JS/Homework6/app.js
+++ Node.JS/Homework6/app.js
@@ -5,6 +5,8 @@ const path = require("path");
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 
 const dataPath = path.join(__dirname, "data", "student.json");
 
