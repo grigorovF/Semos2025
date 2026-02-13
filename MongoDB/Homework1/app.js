7b27bb855ddad848506eda5dc06eef270f65ee03
Filip Grigorov <filipgrigorov44@gmail.com>
2026-02-13 12:45:08 +0100

    Mongo1

--- MongoDB/Homework1/app.js
+++ MongoDB/Homework1/app.js
@@ -15,6 +15,12 @@ mongoose.connect('mongodb://localhost:27017/niva').
     then(()=>console.log('Connected to MongoDB'))
     .catch((err)=>console.log(err));
 
+mongoose.connect("mongodb+srv://username:password@cluster0.xxxxxx.mongodb.net/Semos1.zemjodelie?retryWrites=true&w=majority")
+  .then(() => console.log("Connected to MongoDB on cluster"))
+  .catch((err) => console.log(err));
+
+        
+
 
 app.post("/niva", addNivaController.dodajNiva);
 app.get("/niva", addNivaController.prikaziNivi);
