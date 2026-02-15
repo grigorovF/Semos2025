a0aaab3421f36c5b7ece363a2c3dd90b8657414a
Filip Grigorov <filipgrigorov44@gmail.com>
2026-02-15 14:36:33 +0100

    controller final

--- MongoDB/Day1/controller/blogController.js
+++ MongoDB/Day1/controller/blogController.js
@@ -17,3 +17,95 @@ exports.createBlog = async (req, res) => {
     });
   }
 };
+
+exports.deleteBlog = async (req, res) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(501).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
+exports.updateBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(201).json({
+        status: "Success",
+        data:{
+            blog,
+        }
+    });
+  } catch (err) {
+    res.status(501).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
+
+exports.getBlog = async (req, res) => {
+    try{
+        const blogId = req.params.id;
+        const blog = await Blog.findById(blogId);
+        res.status(201).json({
+            status: "success",
+            data: {blog},
+        });
+    }
+    catch(err){
+        res.status(501).json({
+            status: "fail",
+            message: err.message,
+        })
+    }
+};
+
+exports.getAllBlogs = async(req, res) => {
+    try{
+        const blogs = await Blog.find();
+        res.status(201).json({
+            status: "Success",
+            data: {blogs},
+
+        });
+        //dali tuka, moze da se napravi req.params.od nekoj value, dali range, dali textbox(vo nekoja pretopostavena struktuirana stranica)
+        //i da gi filtrira po tie parametri
+        //req.ratingMinimum.params = 2.0
+        /*try {
+          let filter = {};
+
+          if (req.query.ratingMinimum) {
+            filter.rating = { $gte: Number(req.query.ratingMinimum) };
+          }
+
+          const blogs = await Blog.find(filter);
+
+          res.status(200).json({
+            status: "success",
+            results: blogs.length,
+            data: { blogs },
+          });
+        } catch (err) {
+          res.status(500).json({
+            status: "fail",
+            message: err.message,
+          });
+        }*/
+    }    catch(err){
+      res.status(501).json({
+        status: "fail",
+        message: err.message,
+      });
+    }
+}
