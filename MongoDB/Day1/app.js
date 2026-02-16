d33368d7948c35117e569c6e96987867762e7e11
Filip Grigorov <filipgrigorov44@gmail.com>
2026-02-16 16:04:43 +0100

    bez patch

--- MongoDB/Day1/app.js
+++ MongoDB/Day1/app.js
@@ -1,18 +1,31 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const ejsController = require("./controller/ejsController")
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 
-const localDB = mongoose.createConnection("mongodb://127.0.0.1:27017/blogs");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/blogs")
+  .then(() => console.log("Connected to LOCAL database"))
+  .catch((err) => console.log(err));
 
-localDB.on("connected", () => {
-  console.log("Connected to LOCAL database");
+
+
+app.get("/singleBlog/:id", ejsController.getOneBlog);
+
+app.get("/addBlogs", (req, res) => {
+  res.render("addBlogs");
 });
 
+app.post("/addBlogs", ejsController.createBlog);
+app.get("/getAllBlogs", ejsController.getAllBlogs);
+
+app.delete("/deleteBlog/:id", ejsController.deleteBlog);
 
 app.listen(10000, (err) => {
   if (err) console.log(err.message);
--- MongoDB/Day1/controller/ejsController.js
+++ MongoDB/Day1/controller/ejsController.js
@@ -15,7 +15,8 @@ exports.getLanding = async (req, res) => {
 exports.getAllBlogs = async (req, res) => {
   try {
     const blogs = await Blog.find();
-    res.render("allBlogs", {
+   
+    res.render("getAllBlogs", {
       blogs: blogs,
     });
   } catch (err) {
@@ -28,27 +29,30 @@ exports.getOneBlog = async (req, res) => {
   try {
     const blogId = req.params.id;
     const blog = await Blog.findById(blogId);
-    res.render("getOneBlog", {
-      data: blog,
-    });
+    // console.log(blogId);
+
+    // if (!blog) {
+    //   return res.status(404).send("Blog not found");
+    // }
+    res.render("getOneBlog", { blog });
   } catch (err) {
-    console.log(err.message);
+    console.log("ERROR FULL:", err);
     res.status(500).send("error with DB");
   }
 };
 
+
 exports.createBlog = async (req, res) => {
   try {
     await Blog.create(req.body);
-    res.redirect("/api/v1/landing");
+    res.redirect("/getAllBlogs");
   } catch (err) {
     console.error(err);
     res.status(500).render("error", {
-      message: "Something went wrong",
+      message: err.message,
     });
   }
 };
-
 exports.deleteBlog = async (req, res) => {
   try {
     const blogId = req.params.id;
