556f2bdb1f9ef041c5144d70d1a63acb6820663b
Filip Grigorov <filipgrigorov44@gmail.com>
2026-03-05 18:36:06 +0100

    Delete Express directory

--- Express/agencija/app.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const db = require("./pkg/db/index");
-const trips = require("./handlers/tirps/trips");
-const reservationRoutes = require("./routes/reservations");
-
-app.use("/api/reservations", reservationRoutes);
