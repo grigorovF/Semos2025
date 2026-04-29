bc1ae17282943c4c40517c20d3a5bb5479cd4834
Filip Grigorov <filipgrigorov44@gmail.com>
2026-04-29 19:47:36 +0200

    h6

--- React/Homework6/src/components/Albums.jsx
+++ React/Homework6/src/components/Albums.jsx
@@ -4,10 +4,6 @@ import { AlbumsContext } from "../utils/AlbumsContext";
 export const Albums = () => {
   const { albums } = useContext(AlbumsContext);
 
-  if (!albums || albums.length === 0) {
-    return <h2>Loading...</h2>;
-  }
-
   return (
     <div>
       <h2>Albums</h2>
